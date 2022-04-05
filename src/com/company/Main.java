5d10cbad44b5e5dfe64c40ab221917d103dfc7fd
LT2BS25839\Alumne_mati1 <pp@example.com>
2022-04-05 12:58:23 +0200

    commit #1 : feature/b1 subida de rama

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,49 +1,60 @@
 package com.company;
-
 import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        BirdDatabase db = new BirdDatabase();
-
-        while (true) {
-            String command = ask(scanner, "Option?");
-            if (command.equals("Quit")) {
-                break;
-            } else if (command.equals("Add")) {
-                add(scanner,db);
-            } else if (command.equals("Observation")) {
-                observation(scanner,db);
-            } else if (command.equals("Show")) {
-                show(scanner,db);
-            } else if (command.equals("Statistics")) {
-                statistics(scanner,db);
-            } else {
-                System.out.println("Unknown command!");
+        BirdDatabase birdModel = new BirdDatabase();
+        boolean option = true;
+        while (option) {
+            System.out.println("0 Quit\n 1 Add\n 2 Observation\n 3 Show\n 4 Statistics\n");
+            String command = ask(scanner);
+            switch (command){
+                case  "0" :
+                    option = false;
+                    break;
+                case "1" :
+                    add(scanner,birdModel);
+                    break;
+                case "2" :
+                    observation(scanner,birdModel);
+                    break;
+                case "3":
+                    show(scanner,birdModel);
+                    break;
+                case "4" :
+                    statistics(scanner,birdModel);
+                    break;
+                default:
+                    System.out.println("Unknown Option!");
             }
         }
     }
 
-    public static String ask(Scanner input, String option) {
-        return null;
+    public static String ask(Scanner input) {
+        String option = input.nextLine();
+        return option;
     }
 
-    public static void add(Scanner input, BirdDatabase db) {
+    public static void add(Scanner input, BirdDatabase birdModel) {
+        System.out.println("Name:");
+        String name = input.nextLine();
+        System.out.println("Name latin:");
+        String nameLatin = input.nextLine();
+        System.out.println("Observations:");
+        String observations = input.nextLine();
+    }
+
+    public static void observation(Scanner input, BirdDatabase birdModel) {
 
     }
 
-    public static void observation(Scanner input, BirdDatabase db) {
+    public static void show(Scanner input, BirdDatabase birdModel) {
 
     }
 
-    public static void show(Scanner input, BirdDatabase db) {
-
-    }
-
-    public static void statistics(Scanner input, BirdDatabase db) {
+    public static void statistics(Scanner input, BirdDatabase birdModel) {
 
     }
 
