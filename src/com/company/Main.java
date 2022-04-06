d30bd043ed2cd0d364243e9550b23884bd5b27f3
Alumne_mati1 <ipalomon@gmail.com>
2022-04-06 09:45:15 +0200

    commit #1 : feature/b1 Actualizacion Main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,31 +10,19 @@ public class Main {
         while (option) {
             System.out.println("0 Quit\n 1 Add\n 2 Observation\n 3 Show\n 4 Statistics\n");
             String command = ask(scanner);
-            switch (command){
-                case  "0" :
-                    option = false;
-                    break;
-                case "1" :
-                    add(scanner,birdModel);
-                    break;
-                case "2" :
-                    observation(scanner,birdModel);
-                    break;
-                case "3":
-                    show(scanner,birdModel);
-                    break;
-                case "4" :
-                    statistics(scanner,birdModel);
-                    break;
-                default:
-                    System.out.println("Unknown Option!");
+            switch (command) {
+                case "0" -> option = false;
+                case "1" -> add(scanner, birdModel);
+                case "2" -> observation(scanner, birdModel);
+                case "3" -> show(scanner, birdModel);
+                case "4" -> statistics(scanner, birdModel);
+                default -> System.out.println("Unknown Option!");
             }
         }
     }
 
     public static String ask(Scanner input) {
-        String option = input.nextLine();
-        return option;
+        return input.nextLine();
     }
 
     public static void add(Scanner input, BirdDatabase birdModel) {
